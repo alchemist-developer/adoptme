4a031fd729e0ba633ec8ba19238764b9b64cc43d
Luiza Trocino <luizatrocino@hotmail.com>
2022-06-27 22:35:17 -0300

    add petservices

--- BackEnd/src/domain/pets/controllers/petController.js
+++ BackEnd/src/domain/pets/controllers/petController.js
@@ -1,47 +1,15 @@
 const { Pet } = require("../../../database/models");
-const cloudinary = require("../../../configs/cloudinary")
-const fs = require('fs')
+const PetService = require('../services/petService')
 
 const PetController = {
   async create(req, res) {
     try {
       
-      const {user_id: user_id,} = req.auth;
-
-
+      const {user_id} = req.auth;
       const file = req.files
-       
-        if(file.length==1){
-          const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-          image_pet01=uploadPathOne.imageUrl.substr(52,50)
-          image_pet02=null
-          image_pet03=null
-          fs.unlinkSync(file[0].path);
-        }
-
-        if(file.length==2){
-          const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-          const uploadPathTwo = await cloudinary.uploads(file[1].path,'adoptme/pets')
-          image_pet01=uploadPathOne.imageUrl.substr(52,50)
-          image_pet02=uploadPathTwo.imageUrl.substr(52,50)
-          image_pet03=null
-          fs.unlinkSync(file[0].path);
-          fs.unlinkSync(file[1].path);
-        }
-
-        if(file.length==3){
-          const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-          const uploadPathTwo = await cloudinary.uploads(file[1].path,'adoptme/pets')
-          const uploadPathThree = await cloudinary.uploads(file[2].path,'adoptme/pets')
-          image_pet01=uploadPathOne.imageUrl.substr(52,50)
-          image_pet02=uploadPathTwo.imageUrl.substr(52,50)
-          image_pet03=uploadPathThree.imageUrl.substr(52,50)
-          fs.unlinkSync(file[0].path);
-          fs.unlinkSync(file[1].path);
-          fs.unlinkSync(file[2].path);
-        }
-        
-
+      
+      const {image_pet01, image_pet02, image_pet03} = await PetService.registerImages(file)
+      
       const newPet = await Pet.create({
         ...req.body,
         user_id,
@@ -51,20 +19,19 @@ const PetController = {
         image_pet03
       });
 
-      res.json(newPet);
+      res.status(201).json(newPet);
     } catch (error) {
       res.status(500).json("Não foi possível publicar o Pet");
-      console.error(error);
+      console.log(error)
     }
   },
 
   async listAllPets(req, res) {
     try {
       const listPets = await Pet.findAll();
-      res.status(201).json(listPets);
+      res.status(200).json(listPets);
     } catch (error) {
       res.status(500).json("Falha ao listar os pets");
-      console.error(error);
     }
   },
 
@@ -74,71 +41,27 @@ const PetController = {
       const { user_id } = req.auth;
       const file = req.files
     
-      const petByUser = await Pet.count({
-        where: {
-          pet_id,
-          user_id,
-        },
-      });
+      const petByUser = await PetService.findPetByUser(user_id,pet_id)
       
       if (!petByUser) {
         return res.status(404).json("Pet não existe ou não pertence a este usuário");
       }
 
-      const findPet = await Pet.findByPk(pet_id);
-
-      if(file.length==1){
-        const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-        image_pet01=uploadPathOne.imageUrl.substr(52,50)
-        image_pet02=findPet.image_pet02
-        image_pet03=findPet.image_pet03
-        fs.unlinkSync(file[0].path);
-      }
-
-      if(file.length==2){
-        const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-        const uploadPathTwo = await cloudinary.uploads(file[1].path,'adoptme/pets')
-        image_pet01=uploadPathOne.imageUrl.substr(52,50)
-        image_pet02=uploadPathTwo.imageUrl.substr(52,50)
-        image_pet03=findPet.image_pet03
-        fs.unlinkSync(file[0].path);
-        fs.unlinkSync(file[1].path);
-      }
-
-      if(file.length==3){
-        const uploadPathOne = await cloudinary.uploads(file[0].path,'adoptme/pets')
-        const uploadPathTwo = await cloudinary.uploads(file[1].path,'adoptme/pets')
-        const uploadPathThree = await cloudinary.uploads(file[2].path,'adoptme/pets')
-        image_pet01=uploadPathOne.imageUrl.substr(52,50)
-        image_pet02=uploadPathTwo.imageUrl.substr(52,50)
-        image_pet03=uploadPathThree.imageUrl.substr(52,50)
-        fs.unlinkSync(file[0].path);
-        fs.unlinkSync(file[1].path);
-        fs.unlinkSync(file[2].path);
-      }
-
+      const {image_pet01, image_pet02, image_pet03} = await PetService.updateImages(file, pet_id)
 
       await Pet.update(
-        {
-          ...req.body,
+        { ...req.body,
           image_pet01,
           image_pet02,
-          image_pet03
-        }
-        ,
-        {
-          where: {
-            pet_id,
-          },
-        }
+          image_pet03},
+        { where: {pet_id,},}
       );
       
-      const updatedPet = await Pet.findByPk(pet_id);
-      
+      const updatedPet = await PetService.findPet(pet_id)
       res.status(200).json(updatedPet);
+
     } catch (error) {
       res.status(500).json("Erro ao atualizar o pet");
-      console.error(error);
     }
   },
 
@@ -147,18 +70,13 @@ const PetController = {
       const { pet_id } = req.params;
       const { user_id } = req.auth;
 
-      const petByUser = await Pet.count({
-        where: {
-          pet_id,
-          user_id,
-        },
-      });
+      const petByUser = await PetService.findPetByUser(user_id,pet_id)
 
       if (!petByUser) {
         return res.status(404).json("Erro ao encontrar o pet!");
       }
 
-      const findPet = await Pet.findByPk(pet_id)
+      const findPet = await PetService.findPet(pet_id)
       if(!findPet.status){
           return res.status(401).json('Pet já desativado')
       }
@@ -172,14 +90,11 @@ const PetController = {
           }
         }
       );
-
       
-      const updatedPet = await Pet.findByPk(pet_id)
-
+      const updatedPet = await PetService.findPet(pet_id)
       return res.status(200).json(updatedPet);
     } catch (error) {
       res.status(500).json("Falha ao deletar cadastro do Pet");
-      console.error(error);
     }
   },
 };
