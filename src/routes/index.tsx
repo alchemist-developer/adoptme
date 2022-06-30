39a2316152a60db5d7b62c9c0f449e7327022b4a
Adryel Simon <adryel@live.it>
2022-06-30 09:48:49 -0300

    rota contato

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,20 @@
 import {
-    Routes as WrapperRoutes,
-    Route,
-    BrowserRouter,
-  } from "react-router-dom";
-  import AdotarPet from "../pages/AdotarPet";
-  
-  
-  function Routes(): JSX.Element {
-    return (
-      <BrowserRouter>
-        <WrapperRoutes>
-  
-          <Route path="/adotar" element={<AdotarPet />} />
-        </WrapperRoutes>
-      </BrowserRouter>
-    );
-  }
-  
-  export default Routes;
+  Routes as WrapperRoutes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import AdotarPet from "../pages/AdotarPet";
+import Contatar from "../pages/Contatar";
+
+function Routes(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <WrapperRoutes>
+        <Route path="/adotar" element={<AdotarPet />} />
+        <Route path="/contato" element={<Contatar />} />
+      </WrapperRoutes>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
