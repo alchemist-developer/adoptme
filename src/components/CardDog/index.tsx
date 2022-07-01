230ac7a9d0c7d922bb0a6e03f9c7139d64bdf828
Adryel Simon <adryel@live.it>
2022-07-01 00:47:59 -0300

    componente swiper caes adaptado nova versao ui

--- src/components/CardDog/index.tsx
+++ src/components/CardDog/index.tsx
@@ -17,31 +17,38 @@ const CardDog: React.FC = () => (
         <div><img src={Foto} alt="" /></div>
         <div className="info-container">
             <div className="info-dog-column">
+            <div className="button-hover"><button className='btn-fromtop'>⮝</button></div>
                 <div className="dog-nome-row">
                     <h1>Bob</h1>
-                    <Button className="compartilhar-btn">compartilhar</Button>
+                    <button className="compartilhar-btn">➲ compartilhar</button>
                 </div>
                 <p className="doador">ONG Proteger</p>
                 <p className="local">Penha, São Paulo, BR.</p>
-                <div className="caracteristicas-row-container">
-                    <div className="caracteristica-item">Experiente</div>
-                    <div className="caracteristica-item">Macho</div>
-                    <div className="caracteristica-item">Yorkshire</div>
+                <div className="row-container">
+                    <div className="row-item1">Informação</div>
+                    <div className="row-item2">Informação</div>
+                    <div className="row-item3">Informação</div>
+                    <div className="row-item4">Informação</div>
                 </div>
                 <p className="descricao">O Bob é um cachorro resgatado na rua. Acreditamos que foi abandonado por uma família nos arredores da ONG de propósito. Está conosco há 2 meses e ainda é um pouco desconfiado e sente saudades da antiga família.</p>
-                <div className="personalidade-row-container">
-                    <div className="personalidade-item">Arisco</div>
-                    <div className="personalidade-item">Desconfiado</div>
-                </div>
                 <div className="observacoes">
                     <p>Observações importantes</p>
                     <ul>
                         <li>Precisa de espaço para brincar</li>
                         <li>É desconfiada</li>
+                        <li>É desconfiada</li>
+                        <li>É desconfiada</li>
                     </ul>
                 </div>
+                <div className="row-container">
+                    <div className="row-item1">Informação</div>
+                    <div className="row-item2">Informação</div>
+                    <div className="row-item3">Informação</div>
+                    <div className="row-item4">Informação</div>
+                </div>
+                
             </div>
-            <button className='btn-contatar'>Contatar doador </button>
+            <button className='btn-contatar'>Adotar pet  <span>♥</span></button>
         </div>
         {/* botão para adotar, useState para alterar Interesse x Adotar
          no collapse*/}
