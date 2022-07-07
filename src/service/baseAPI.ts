37166c6ec3efed8aa03914ef0e27b3e5a5f4d226
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-07-06 23:09:41 -0300

    finalizando parte logica do componente querodoar, ligando com  a api

--- src/service/baseAPI.ts
+++ src/service/baseAPI.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const baseAPI = axios.create({
     baseURL: "https://adoptme-app.herokuapp.com",
-    headers: {
-        "content-type": "application/json",
-    },
+    // headers: {
+    //     "content-type": "application/json",
+    // },
 });
 export const baseAPICreated = axios.create({
     baseURL: "https://adoptme-app.herokuapp.com",
