711bd95ce3b0b89ba75743d1de196d96a43cc693
Adryel Simon <adryel@live.it>
2022-07-08 17:37:47 -0300

    card style adjust

--- src/components/InfoDog/styled.js
+++ src/components/InfoDog/styled.js
@@ -2,13 +2,14 @@ import styled, { css } from "styled-components";
 
 export const InfoContainer = styled.div`
   position: fixed;
-  transform: translateY(89%);
+  transform: translateY(86%);
   width: 100%;
   height: 90%;
   background-image: linear-gradient(
     to bottom,
-    rgba(255, 255, 255, 0.6),
-    rgba(255, 255, 255, 1) 20%
+    rgba(255, 255, 255, 0.0),
+    rgba(255, 255, 255, 0.66),
+    rgba(255, 255, 255, 0.89) 20%
   );
   backdrop-filter: blur(2px);
   display: flex;
