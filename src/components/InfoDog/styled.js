3c29b0be9f19f87115c77176cd90fd16f2bc9e84
Adryel Simon <adryel@live.it>
2022-07-09 16:48:35 -0300

    transform adjust xiomi 12, iphone 11 pro

--- src/components/InfoDog/styled.js
+++ src/components/InfoDog/styled.js
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 
 export const InfoContainer = styled.div`
   position: fixed;
-  transform: translateY(91%);
+  transform: translateY(89%);
   width: 100%;
   height: 90%;
   background-image: linear-gradient(
@@ -26,9 +26,9 @@ export const InfoContainer = styled.div`
       flex-direction: column;
       align-items: center;
       position: fixed;
-      transform: translateY(10%);
+      transform: translateY(11%);
       width: 100%;
-      height: 93%;
+      height: 90%;
     `}
 `;
 
