a6898bd718b0f8cd42481e4e47b897df65365dd1
Junior Dering <juniordering@hotmail.com>
2022-07-10 20:20:35 -0400

    melhorando carousel e menus

--- src/Components/CarouselTestemunho/index.tsx
+++ src/Components/CarouselTestemunho/index.tsx
@@ -9,22 +9,22 @@ const CarouselTestemunho= () => {
         <Carousel className="carousel-body" controls = {false} indicators = {true}>
           <Carousel.Item className="carouselItem">
             <img
-              className="d-block w-100 carosuel-body"
+              className="d-block w-100 carosuel-body-testemunho"
               src={carrosselTestemunho1}
               alt="First slide"
             />
-            <h4>Patricia Meireles</h4>
-            <p>Essa fofura animal parece que estava só nos esperando pra formarmos uma família completa. Encontramos a Mia, através da plataforma me.adota, e depois de várias conversas com os protetores dela, Seu Januário e sua filha Michele, nós finalmente a levamos para casa. Hoje já está totalmente habituada a rotina da família, se sente confortável até pra fazer travessuras.</p>
+            <h4 className='text'>Patricia Meireles</h4>
+            <p className='text' >Essa fofura animal parece que estava só nos esperando pra formarmos uma família completa. Encontramos a Mia, através da plataforma me.adota, e depois de várias conversas com os protetores dela, Seu Januário e sua filha Michele, nós finalmente a levamos para casa. Hoje já está totalmente habituada a rotina da família, se sente confortável até pra fazer travessuras.</p>
 
           </Carousel.Item>
           <Carousel.Item className="carouselItem">
             <img
-                className="d-block w-100 carosuel-body"
+                className="d-block w-100 carosuel-body-testemunho"
                 src={carrosselTestemunho2}
                 alt="First slide"
               />    
-        
-              <p>Pai de pet, real oficial! Agora minha trilha sonora são miados dessas duas tagarelas que eu amo muito. Já fazem 3 meses que a Lara e a Lua estão comigo, encontrei o protetor delas, o Maurício do Projeto Patinhas, através da plataforma me.adota, foi super de boa... o processo de adoção e adaptação também, tudo supervisionado pelo projeto voluntário. Tô muito feliz!</p>
+              <h4 className='text'>Carlos Gabriel</h4>
+              <p className='text'>Pai de pet, real oficial! Agora minha trilha sonora são miados dessas duas tagarelas que eu amo muito. Já fazem 3 meses que a Lara e a Lua estão comigo, encontrei o protetor delas, o Maurício do Projeto Patinhas, através da plataforma me.adota, foi super de boa... o processo de adoção e adaptação também, tudo supervisionado pelo projeto voluntário. Tô muito feliz!</p>
             
           </Carousel.Item>
       </Carousel>
